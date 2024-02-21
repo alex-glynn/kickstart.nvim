ff86712863b1d722f025efacda29750807cd5efc
Alex <alex.glynn@louisville.edu>
2024-02-21 16:23:50 -0500

    Update

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ vim.g.copilot_assume_mapped = true
 vim.g.R_assign = 0
 vim.g.R_rconsole_width = 80
 vim.g.R_min_editor_width = 18
+vim.opt.conceallevel = 1
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -38,6 +39,22 @@ require('lazy').setup({
   'tpope/vim-rhubarb',
   'github/copilot.vim',
 
+  -- Command line
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
@@ -51,31 +68,31 @@ require('lazy').setup({
   -- LaTeX
   'lervag/vimtex',
   {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    -- tag = "*",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-          ['core.export'] = {},
-          ['core.export.markdown'] = {},
-          ['core.esupports.metagen'] = {}
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = {'nvim-tree/nvim-web-devicons'},
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      -- Required
+      "nvim-lua/plenary.nvim",
+      -- Optional
+      "hrsh7th/nvim-cmp", -- completion of note references
+      "nvim-telescope/telescope.nvim", -- search and quick-switch
+      "nvim-treesitter/nvim-treesitter" -- syntax highlighting
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "main",
+          path = "~/vaults/main",
         },
-      }
-      vim.wo.foldlevel = 99
-      vim.wo.conceallevel = 2
-    end,
+      },
+    },
   },
   {
     'catppuccin/nvim',
@@ -103,25 +120,38 @@ require('lazy').setup({
   },
   -- quarto
   {
-    'quarto-dev/quarto-nvim',
-    dev = false,
-    dependencies = {
-      {
-        'jmbuhr/otter.nvim',
-        dev = false,
-        dependencies = {
-          { 'neovim/nvim-lspconfig' },
-        },
-        opts = {
-          buffers = {
-            set_filetype = true,
-          },
-        },
-      },
-    },
+    "quarto-dev/quarto-nvim",
     opts = {
       lspFeatures = {
-        languages = { 'r', 'python', 'julia', 'bash', 'lua', 'html' },
+        languages = { "r", "python", "julia", "bash", "html", "lua" },
+      },
+    },
+    ft = "quarto",
+    keys = {
+      { "<leader>qa", ":QuartoActivate<cr>", desc = "quarto activate" },
+      { "<leader>qp", ":lua require'quarto'.quartoPreview()<cr>", desc = "quarto preview" },
+      { "<leader>qq", ":lua require'quarto'.quartoClosePreview()<cr>", desc = "quarto close" },
+      { "<leader>qh", ":QuartoHelp ", desc = "quarto help" },
+      { "<leader>qe", ":lua require'otter'.export()<cr>", desc = "quarto export" },
+      { "<leader>qE", ":lua require'otter'.export(true)<cr>", desc = "quarto export overwrite" },
+      { "<leader>qrr", ":QuartoSendAbove<cr>", desc = "quarto run to cursor" },
+      { "<leader>qra", ":QuartoSendAll<cr>", desc = "quarto run all" },
+      { "<leader><cr>", ":SlimeSend<cr>", desc = "send code chunk" },
+      { "<c-cr>", ":SlimeSend<cr>", desc = "send code chunk" },
+      { "<c-cr>", "<esc>:SlimeSend<cr>i", mode = "i", desc = "send code chunk" },
+      { "<c-cr>", "<Plug>SlimeRegionSend<cr>", mode = "v", desc = "send code chunk" },
+      { "<cr>", "<Plug>SlimeRegionSend<cr>", mode = "v", desc = "send code chunk" },
+      { "<leader>ctr", ":split term://R<cr>", desc = "terminal: R" },
+      { "<leader>cti", ":split term://ipython<cr>", desc = "terminal: ipython" },
+      { "<leader>ctp", ":split term://python<cr>", desc = "terminal: python" },
+      { "<leader>ctj", ":split term://julia<cr>", desc = "terminal: julia" },
+    },
+  },
+  {
+    "jmbuhr/otter.nvim",
+    opts = {
+      buffers = {
+        set_filetype = true,
       },
     },
   },
@@ -133,6 +163,13 @@ require('lazy').setup({
     config = function()
       require('nvim-treesitter.configs').setup({
         ensure_installed = {
+          'bash',
+          'c',
+          'javascript',
+          'json',
+          'markdown',
+          'markdown_inline',
+          'regex',
           'r',
           'python',
           'markdown',
@@ -575,10 +612,54 @@ require('lazy').setup({
       { "L3MON4D3/LuaSnip" },
       { "rafamadriz/friendly-snippets" },
       { "onsails/lspkind-nvim" },
-
       -- optional
       -- more things to try:
     },
+    -- opts = function(_, opts)
+    -- local cmp = require('cmp')
+    -- -- opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "otter" } }))
+    -- end,
+  },
+  -- send code from python/r/qmd documets to a terminal or REPL
+  -- like ipython, R, bash
+  {
+    "jpalardy/vim-slime",
+    init = function()
+      vim.b["quarto_is_" .. "python" .. "_chunk"] = false
+      Quarto_is_in_python_chunk = function()
+        require("otter.tools.functions").is_otter_language_context("python")
+      end
+
+      vim.cmd([[
+      let g:slime_dispatch_ipython_pause = 100
+      function SlimeOverride_EscapeText_quarto(text)
+      call v:lua.Quarto_is_in_python_chunk()
+      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk
+      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+      else
+      return a:text
+      end
+      endfunction
+      ]])
+
+      local function mark_terminal()
+        vim.g.slime_last_channel = vim.b.terminal_job_id
+        vim.print(vim.g.slime_last_channel)
+      end
+
+      local function set_terminal()
+        vim.b.slime_config = { jobid = vim.g.slime_last_channel }
+      end
+
+      -- slime, neovvim terminal
+      vim.g.slime_target = "neovim"
+      vim.g.slime_python_ipython = 1
+
+      require("which-key").register({
+        ["<leader>cm"] = { mark_terminal, "mark terminal" },
+        ["<leader>cs"] = { set_terminal, "set terminal" },
+      })
+    end,
   },
   -- paste an image to markdown from the clipboard
   -- :PasteImg,
@@ -612,6 +693,22 @@ require('lazy').setup({
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
     },
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        pyright = {},
+        r_language_server = {},
+        julials = {},
+        --marksman = {
+        -- also needs:
+        -- $home/.config/marksman/config.toml :
+        -- [core]
+        -- markdown.file_etensions = ["md", "markdown", "qmd"]
+        --filetypes = { "markdown", "quarto" },
+        --root_dir = require("lspconfig.util").root_pattern(".git", ".marksman.toml", "_quarto.yml"),
+        --},
+      },
+    }
   },
 
   {
@@ -789,6 +886,9 @@ vim.o.termguicolors = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Toggle conceallevel
+vim.keymap.set({ 'n', 'v' }, '<leader>cl', ':setlocal conceallevel=<C-R>=&conceallevel == 0 ? "1" : "0"<CR><CR>', { desc = 'Toggle conceallevel' })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
